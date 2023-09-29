aeb8ee8da3604d1dc6f76357e67a9228b7af1d83
[Hailey G] <[haileydgrace@gmail.com]>
2023-09-28 21:15:36 -0400

    css

--- script.js
+++ script.js
@@ -27,21 +27,21 @@ let futureTemp = {
 //todays weather object information
 let todayTemp = {
         currentTemp: "",
-    };
+};
 let today = document.getElementsByClassName("today");
 
 
 //activate eventlistener on search button
- let search = document.getElementById("searchButton");
+let search = document.getElementById("searchButton");
         search.addEventListener("click", function () {
 
 //Store the search input value into cityFromUser variable
- let cityFromUser = document.getElementById("searchInput").value;
+let cityFromUser = document.getElementById("searchInput").value;
 
 //remove spaces and replace with - so the link works properly
         if (cityFromUser.includes(" ")) {
-            cityFromUser = cityFromUser.replace(/ /g, "-");
-            console.log(cityFromUser);
+        cityFromUser = cityFromUser.replace(/ /g, "-");
+        console.log(cityFromUser);
         }
 
 //attach the input value into it's proper location in the API link
@@ -61,8 +61,8 @@ let today = document.getElementsByClassName("today");
 
         todayTemp.currentTemp = data.current.temp_f;
         today.textContent = todayTemp.currentTemp;
-            
-            
+        
+        
 
  //make a shorter variable to hold the initial API call line
         dayCall = data.forecast.forecastday;
@@ -74,15 +74,15 @@ let today = document.getElementsByClassName("today");
         date = dayCall[i].date;
         
         futureTemp[date] = {
-            maxtemp_fz: `${dayCall[i].day.maxtemp_f}`,
-            maxtemp_c: `${dayCall[i].day.maxtemp_c}`,
-            mintemp_f: `${dayCall[i].day.mintemp_f}`,
-            mintemp_c: `${dayCall[i].day.mintemp_c}`,
-            avgtemp_c: `${dayCall[i].day.avgtemp_c}`,
-            avgtemp_f: `${dayCall[i].day.avgtemp_f}`,
-            daily_chance_of_rain: `${dayCall[i].day.daily_chance_of_rain}`,
-            condition_icon: `${dayCall[i].day.condition.icon}`,
-            condition_text: `${dayCall[i].day.condition.text}`,
+        maxtemp_f: `${dayCall[i].day.maxtemp_f}`,
+        maxtemp_c: `${dayCall[i].day.maxtemp_c}`,
+        mintemp_f: `${dayCall[i].day.mintemp_f}`,
+        mintemp_c: `${dayCall[i].day.mintemp_c}`,
+        avgtemp_c: `${dayCall[i].day.avgtemp_c}`,
+        avgtemp_f: `${dayCall[i].day.avgtemp_f}`,
+        daily_chance_of_rain: `${dayCall[i].day.daily_chance_of_rain}`,
+        condition_icon: `${dayCall[i].day.condition.icon}`,
+        condition_text: `${dayCall[i].day.condition.text}`,
 }
 }
 })
