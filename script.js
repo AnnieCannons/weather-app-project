e1d3a75dfdb24081dafdbd1eee85e8293ee2e902
[Hailey G] <[haileydgrace@gmail.com]>
2023-10-02 13:51:57 -0400

    Changes

--- script.js
+++ script.js
@@ -12,10 +12,14 @@ let future1 = document.getElementById("1");
 let future2 = document.getElementById("2");
 let future3 = document.getElementById("3");
 futureResults = [1, 2, 3];
+let celsiusButton = document.querySelector("button.c");
+let fahrenheitButton = document.querySelector("button.f");
+let todaySummary = ''
 
 
 
-// console.log(hiddenElement)
+
+
 
 today = [
         "date",
@@ -62,8 +66,6 @@ document.getElementById("searchButton").addEventListener(
 
                                 dayCall = data.forecast.forecastday;
 
-                             
-                                
 
                                 todayList = [
                                         (document.querySelector(
@@ -86,12 +88,14 @@ document.getElementById("searchButton").addEventListener(
                                         ).textContent = `Feels Like: ${data.current.feelslike_f}°F`),
                                         (document.querySelector(
                                                 `#todaysResults .chance_of_rain`
-                                        ).textContent = `Chance of Rain: ${dayCall[0].day.daily_chance_of_rain}`),
+                                        ).textContent = `Chance of Rain: ${dayCall[0].day.daily_chance_of_rain}%`),
                                 ];
 
-                             
+
 
                                 for (let i = 1; i < 4; i++) {
+                                        maxlist[i] = dayCall[i].day.maxtemp_f;
+                                        maxDate[i] = getDayOfWeek(dayCall[i].date);
                                         let dates = ["zero", "one", "two", "three"];
                                         (document.querySelector(`#${dates[i]} .date`).textContent =
                                                 getDayOfWeek(dayCall[i].date)),
@@ -107,42 +111,13 @@ document.getElementById("searchButton").addEventListener(
                                                 (document.querySelector(
                                                         `#${dates[i]} .chance_of_rain`
                                                 ).textContent = `Chance of Rain: ${dayCall[i].day.daily_chance_of_rain}%`);
+
                                 }
 
-                               
-
-                                for (let i = 0; i < 4; i++) {
-                                        maxlist[i] = dayCall[i].day.maxtemp_f;
-                                        maxDate[i] = getDayOfWeek(dayCall[i].date);
-
-                                        callLibrary[i] = {
-                                                located: `${data.location.name}, ${data.location.region}`,
-                                                todaySummary: data.current.condition.text,
-                                                todayIcon: data.current.condition.icon,
-                                                date: dayCall[i].date,
-                                                name: data.location.name,
-                                                region: data.location.region,
-                                                country: data.location.country,
-                                                currentTemp: `Currently: ${data.current.temp_f}°`,
-                                                feelslike: `Feels Like: ${data.current.feelslike_f}°`,
-                                                maxtemp: `Today's High: ${dayCall[i].day.maxtemp_f}`,
-                                                mintemp: `Todays Low: ${dayCall[i].day.mintemp_f}`,
-                                                avgtemp: `Average Temp: ${dayCall[i].day.avgtemp_f}`,
-                                                chance_of_rain: `Chance of Rain: ${dayCall[i].day.daily_chance_of_rain}%`,
-                                                // condition_icon: `${dayCall[i].day.condition.icon}`,
-                                                condition_text: `Condition: ${dayCall[i].day.condition.text}`,
-                                                sunrise: `Sunrise: ${dayCall[i].astro.sunrise.text}`,
-                                                sunset: `Sunset: ${dayCall[i].astro.sunset.text}`,
-                                                moonphase: `Moonphase: ${dayCall[i].astro.moon_phase}`,
-                                        };
-                                }
-
-                                console.log(maxDate, maxlist);
 
                                 let highestTemp = `The highest temperature this week is ${maxlist[0]} on ${maxDate[0]}!`;
-                                console.log(maxlist);
                                 for (let i = 0; i < maxlist.length; i++) {
-                                        console.log(maxlist[i]);
+                                       
                                         if (maxlist[i] > highestTemp) {
                                                 highestTemp = `The highest temperature this week is ${maxlist[i]} on ${maxDate[i]}!`;
                                                 document.querySelector(`#highestTemp .highestTemp`).textContent =
@@ -154,16 +129,124 @@ document.getElementById("searchButton").addEventListener(
                                         }
                                 }
 
-                               
-                               if(data.current.temp_f >75) {
-                                todaySummary = document.querySelector(`#today_message .todaySummary`).textContent =`Its Hot Today!`;
-                               } else if(data.current.temp_f > 45 || data.current.temp_f <75) {todaySummary = document.querySelector(`#today_message .todaySummary`).textContent =`Its Moderate Today!`;} else if(data.current.temp_f < 45) {
-                                todaySummary = document.querySelector(`#today_message .todaySummary`).textContent =`Its Cold Today!`;
-                               }
-                               
+
+                                if (data.current.temp_f > 75) {
+                                        todaySummary = document.querySelector(`#today_message .todaySummary`).textContent = `Its Hot Today!`;
+                                } else if (data.current.temp_f > 45 || data.current.temp_f < 75) { todaySummary = document.querySelector(`#today_message .todaySummary`).textContent = `Its Moderate Today!`; } else if (data.current.temp_f < 45) {
+                                        todaySummary = document.querySelector(`#today_message .todaySummary`).textContent = `Its Cold Today!`;
+                                }
+
+                        
+                                })
+                        })
+
+
+                                celsiusButton.addEventListener("click", function celsius() {
+
+                                        cityFromUser = document.getElementById("searchInput").value;
+                                        console.log(cityFromUser);
+
+                                        if (cityFromUser.includes(" ")) {
+                                                cityFromUser = cityFromUser.replace(/ /g, "-");
+                                        }
+
+                                        cityLink = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityFromUser}&days=${4}&aqi=no&alerts=no`;
+
+                                        fetch(cityLink)
+                                                .then((response) => response.json())
+                                                .then((data) => {
+                                                        function getDayOfWeek(date) {
+                                                                const dayOfWeek = new Date(date).getDay();
+                                                                return isNaN(dayOfWeek)
+                                                                        ? null
+                                                                        : [
+                                                                                "Sunday",
+                                                                                "Monday",
+                                                                                "Tuesday",
+                                                                                "Wednesday",
+                                                                                "Thursday",
+                                                                                "Friday",
+                                                                                "Saturday",
+                                                                        ][dayOfWeek];
+                                                        }
+
+                                                        dayCall = data.forecast.forecastday;
+
+
+                                                        todayList = [
+                                                                (document.querySelector(
+                                                                        `#todaysResults .date`
+                                                                ).textContent = `Today`),
+                                                                (document.querySelector(
+                                                                        `#todaysResults .located`
+                                                                ).textContent = `${data.location.name}, ${data.location.region}`),
+                                                                (document.querySelector(
+                                                                        `#todaysResults .currentTemp`
+                                                                ).textContent = `Currently: ${data.current.temp_c}°C`),
+                                                                (document.querySelector(
+                                                                        `#todaysResults .maxtemp`
+                                                                ).textContent = `Today's High: ${dayCall[0].day.maxtemp_c}°C`),
+                                                                (document.querySelector(
+                                                                        `#todaysResults .mintemp`
+                                                                ).textContent = `Todays Low: ${dayCall[0].day.mintemp_c}°C`),
+                                                                (document.querySelector(
+                                                                        `#todaysResults .feelslike`
+                                                                ).textContent = `Feels Like: ${data.current.feelslike_c}°C`),
+                                                                (document.querySelector(
+                                                                        `#todaysResults .chance_of_rain`
+                                                                ).textContent = `Chance of Rain: ${dayCall[0].day.daily_chance_of_rain}`),
+                                                        ];
 
 
 
-                        });
-        }
-);
+                                                        for (let i = 1; i < 4; i++) {
+                                                                let dates = ["zero", "one", "two", "three"];
+                                                                (document.querySelector(`#${dates[i]} .date`).textContent =
+                                                                        getDayOfWeek(dayCall[i].date)),
+                                                                        (document.querySelector(
+                                                                                `#${dates[i]} .avetemp`
+                                                                        ).textContent = `Average  Temp: ${dayCall[i].day.avgtemp_c}°C`),
+                                                                        (document.querySelector(
+                                                                                `#${dates[i]} .maxtemp`
+                                                                        ).textContent = `Today's High: ${dayCall[i].day.maxtemp_c}°C`),
+                                                                        (document.querySelector(
+                                                                                `#${dates[i]} .mintemp`
+                                                                        ).textContent = `Todays Low: ${dayCall[i].day.mintemp_c}°C`),
+                                                                        (document.querySelector(
+                                                                                `#${dates[i]} .chance_of_rain`
+                                                                        ).textContent = `Chance of Rain: ${dayCall[i].day.daily_chance_of_rain}%`);
+
+                                                        }
+
+                                                        
+
+                                                        let highestTemp = `The highest temperature this week is ${maxlist[0]} on ${maxDate[0]}!`;
+                                                     
+                                                        for (let i = 0; i < maxlist.length; i++) {
+                                                          ;
+                                                                if (maxlist[i] > highestTemp) {
+                                                                        highestTemp = `The highest temperature this week is ${maxlist[i]} on ${maxDate[i]}!`;
+                                                                        document.querySelector(`#highestTemp .highestTemp`).textContent =
+                                                                                highestTemp;
+                                                                } else {
+                                                                        highestTemp = `The highest temperature this week is ${maxlist[0]} on ${maxDate[0]}!`;
+                                                                        document.querySelector(`#highestTemp .highestTemp`).textContent =
+                                                                                highestTemp;
+                                                                }
+                                                        }
+
+
+                                                        if (data.current.temp_f > 23) {
+                                                                todaySummary = document.querySelector(`#today_message .todaySummary`).textContent = `Its Hot Today!`;
+                                                        } else if (data.current.temp_c > 7 || data.current.temp_c < 23) { todaySummary = document.querySelector(`#today_message .todaySummary`).textContent = `Its Moderate Today!`; } else if (data.current.temp_c < 7) {
+                                                                todaySummary = document.querySelector(`#today_message .todaySummary`).textContent = `Its Cold Today!`;
+                                                        }
+
+
+                                                })
+                                        })
+                        
+                        
+                
+                                                
+                                        
