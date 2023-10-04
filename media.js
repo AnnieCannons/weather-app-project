2fa70e79134ddadb1105852025ac217f965ce50a
[Hailey G] <[haileydgrace@gmail.com]>
2023-10-04 17:48:52 -0400

    essentials completed

--- media.js
+++ media.js
@@ -1,137 +1,44 @@
+let apiKey = "8b78ddff94f24e8087b182603232609";
+let callLibrary = [];
+let maxlist = [];
+let maxDate = [];
+let searched = [];
+let searchHistory = []
+let dayValue = []
+let cityLink;
+let today;
+let future;
+let todayClass;
+let todayValue;
+let futureClass;
+let futureValue;
+let today_message;
+let futureLibrary;
+let todaySummary;
+let max;
+let searchInput;
+let dates;
+let dayCall;
+let highestTemp;
+let future1 = document.getElementById("1");
+let future2 = document.getElementById("2");
+let future3 = document.getElementById("3");
+futureResults = [1, 2, 3];
+let celsiusButton = document.querySelector("button.c");
+let searchList = document.querySelector(".history-button")
+let hiddenConverter = document.querySelector(".hidden-converter");
+let hiddenMaincontent = document.querySelector(".hidden-maincontent");
+let initialHead = document.querySelector(".initial-header");
+let initialBody = document.querySelector(".initial-body");
+let searchlist = document.querySelector('.searchlist');
 
-function getDayOfWeek(date) {
-    const dayOfWeek = new Date(date).getDay();
-    return isNaN(dayOfWeek) ? null :
-            ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
-}
-let todaysList = '';
-let today_message = '';
-let futureList = '';
-let maxlist = ''
-let maxDate = ''
-let callLibrary = ''
+// (document.querySelector(
+// `#${dates[i]} .chance_of_rain`
+// ).textContent = `Chance of Rain: ${dayCall[i].day.daily_chance_of_rain}%`);
 
-todayClass = [
-    'date',
-    'located',
-    'currentTemp',
-    'feelslike',
-    'maxtemp',
-    'mintemp',
-    'chance_of_rain'
-]
+// let futureBox = [
+//     (futureField = document.createElement('div')),
+//     (fieldClass = futureField.classList.add("content, secondary, 2")),
+//     (fieldId = futureField.setAttribute('id', ${dayValue.length}``))
 
-todaysMessageClass = [
-    'todaySummary',
-]
-
-
-futureClass = [
-    'day',
-    'avgtemp',
-    'maxtemp',
-    'mintemp',
-    'chance_of_rain',
-    'condition_text'
-]
-
-
-
-document.getElementById("searchButton").addEventListener("click",
-
-        function onChange() {
-
-
-                let cityFromUser = document.getElementById("searchInput").value;
-                console.log(cityFromUser)
-           
-                if (cityFromUser.includes(" ")) {
-                        cityFromUser = cityFromUser.replace(/ /g, "-");
-                }
-
-                cityLink = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityFromUser}&days=${4}&aqi=no&alerts=no`;
-
-
-           
-                fetch(cityLink)
-                        .then((response) => response.json())
-                        .then((data) => {
-                        
-                               let dayCall = data.forecast.forecastday;
-
-                                for (let i = 1; i < 4; i++) {
-                                    maxlist[i] = dayCall[i].day.maxtemp_f;
-                                    maxDate[i] = getDayOfWeek(dayCall[i].date);
-                                
-                                future[i] = [
-                                    getDayOfWeek(dayCall[i].date),
-                                    `Average Temp: ${dayCall[i].day.avgtemp_f}`,
-                                    `Today's High: ${dayCall[i].day.maxtemp_f}`,
-                                    `Todays Low: ${dayCall[i].day.mintemp_f}`,
-                                    `Chance of Rain: ${dayCall[i].day.daily_chance_of_rain}%`,
-                                    `Condition: ${dayCall[i].day.condition.text}`
-                                ]
-
-                                let futureList = document.querySelectorAll(".secondary, p")
-                                futureList[i].textContent = future[i]
-
-
-
-                                
-                         
-                                
-                                
-                                    callLibrary[i] =
-                                    {
-                                            located: `${data.location.name}, ${data.location.region}`,
-                                            todaySummary: data.current.condition.text,
-                                            todayIcon: data.current.condition.icon,
-                                            day: getDayOfWeek(dayCall[i].date),
-                                            date: dayCall[i].date,
-                                            name: data.location.name,
-                                            region: data.location.region,
-                                            country: data.location.country,
-                                            currentTemp: `Currently: ${data.current.temp_f}°`,
-                                            feelslike: `Feels Like: ${data.current.feelslike_f}°`,
-                                            maxtemp: `Today's High: ${dayCall[i].day.maxtemp_f}`,
-                                            mintemp: `Todays Low: ${dayCall[i].day.mintemp_f}`,
-                                            avgtemp: `Average Temp: ${dayCall[i].day.avgtemp_f}`,
-                                            chance_of_rain: `Chance of Rain: ${dayCall[i].day.daily_chance_of_rain}%`,
-                                            // condition_icon: `${dayCall[i].day.condition.icon}`,
-                                            condition_text: `Condition: ${dayCall[i].day.condition.text}`,
-                                            sunrise: `Sunrise: ${dayCall[i].astro.sunrise.text}`,
-                                            sunset: `Sunset: ${dayCall[i].astro.sunset.text}`,
-                                            moonphase: `Moonphase: ${dayCall[i].astro.moon_phase}`,
-                                
-                                    }
-                                    
-                                } 
-
-                                today = [
-                                    getDayOfWeek(dayCall[0].date),
-                                    `${data.location.name}, ${data.location.region}`,
-                                    `Currently: ${data.current.temp_f}°`,
-                                    `Average Temp: ${dayCall[0].day.avgtemp_f}`,
-                                    `Today's High: ${dayCall[0].day.maxtemp_f}`,
-                                    `Todays Low: ${dayCall[0].day.mintemp_f}`,
-                                    `Feels Like: ${data.current.feelslike_f}°`,
-                                    `Chance of Rain: ${dayCall[0].day.daily_chance_of_rain}`
-                                ]
-
-                                let todaysList = document.querySelectorAll(".primary")
-                                todaysList.textContent = today
-                                
-                            
-                                let todays_message = document.querySelector("todaySummary")
-                                todays_message.textContent = data.current.condition.text;
-                            }
-                            
-                            ) 
-
-                            
-                        }
-
-                            )
-
-                            console.log(todaysList)
-                            console.log(today)
+// ]
